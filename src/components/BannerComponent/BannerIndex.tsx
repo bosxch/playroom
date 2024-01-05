af0b538ea9c8def52c5a5d0a2079cac7f6669939
bosxch <amparocardosobosch@gmail.com>
2024-01-05 12:01:33 -0300

    forgot to hidden checks

--- src/components/BannerComponent/BannerIndex.tsx
+++ src/components/BannerComponent/BannerIndex.tsx
@@ -1,49 +1,78 @@
-import React from 'react';
+import React from "react";
 // import AokiRocket from './AokiRocket';
-import BtnCallToAction from '../BtnCTA';
-import checkImg from '../../../public/images/check.png'
-import Image from 'next/image';
+import BtnCallToAction from "../BtnCTA";
+import checkImg from "../../../public/images/check.png";
+import Image from "next/image";
 const Banner = () => {
-    return (
-        <div className='banner-main-aoki flex flex-col h-full'>
-
-<div className="flex flex-col sm:flex-row h-100 w-100 justify-center banner-container bg-transparent">
-
+  return (
+    <div className="banner-main-aoki flex flex-col h-full">
+      <div className="flex flex-col sm:flex-row h-100 w-100 justify-center banner-container bg-transparent">
         {/* Sección del texto */}
         <div className="w-full p-4 sm:p-8 flex flex-col items-center justify-center text-center text-container-banner">
           <p className="text-white text-xl sm:text-2xl lg:text-4xl font-bold mb-4 font-poppins text-center w-full sm:text-center">
-            Querés <span className="gradient-text">innovación</span>, querés <span className="gradient-text">Aoki</span>
+            Querés <span className="gradient-text">innovación</span>, querés{" "}
+            <span className="gradient-text">Aoki</span>
           </p>
           <h1 className="text-white text-4xl sm:text-6xl lg:text-8xl font-bold mb-8 font-poppins text-center sm:text-center w-full">
-            Subí de nivel <span className="gradient-text">tu empresa</span><br />de la mano de la <span className="gradient-text">IA</span>
+            Subí de nivel <span className="gradient-text">tu empresa</span>
+            <br />
+            de la mano de la <span className="gradient-text">IA</span>
           </h1>
-          <div className='second-title-compu'>
-          <p className="text-white text-lg sm:text-lg lg:text-lg font-bold mb-8 font-poppins text-center w-full sm:text-center">
-          <span className="gradient-text">Automatizarte</span> nunca fue tan <span className="gradient-text">fácil</span>
-          </p>
-          {/* <button className="w-60 btn-bg-color text-white p-2 mb-2 mt-6">Empezá tu prueba gratuita</button> */}
-          <div className='btn-container'>
-          <BtnCallToAction
-          text={'Empezá tu prueba gratuita hoy'}
-          rocket={false}
-          className={"w-60 text-white p-2 mb-2 mt-6 btn-bg-color"}
-          />
+          <div className="second-title-compu">
+            <p className="text-white text-lg sm:text-lg lg:text-lg font-bold mb-8 font-poppins text-center w-full sm:text-center">
+              <span className="gradient-text">Automatizarte</span> nunca fue tan{" "}
+              <span className="gradient-text">fácil</span>
+            </p>
+            {/* <button className="w-60 btn-bg-color text-white p-2 mb-2 mt-6">Empezá tu prueba gratuita</button> */}
+            <div className="btn-container">
+              <BtnCallToAction
+                text={"Empezá tu prueba gratuita hoy"}
+                rocket={false}
+                className={"w-60 text-white p-2 mb-2 mt-6 btn-bg-color"}
+              />
+            </div>
+            <p className="text-white text-sm mb-4">
+              Podes empezar en menos de 60 segundos
+            </p>
           </div>
-          <p className="text-white text-sm mb-4">Podes empezar en menos de 60 segundos</p>
-          </div>
-          <div className='flex flex-col sm:flex-row justify-center items-center w-full p-4 sm:space-x-8 absolute bottom-0 items-banner mr-8'>
-            <div className='flex flex-row items-center text-white pb-8' style={{marginLeft: 0}}>
-              <Image src={checkImg} alt='check' height={30} width={30} className='mr-2' />
+          <div className="flex flex-col sm:flex-row justify-center items-center w-full p-8 sm:space-x-8 absolute bottom-0 items-banner mr-8 ">
+            <div
+              className="flex flex-row items-center text-white pb-8"
+              style={{ marginLeft: 0 }}
+            >
+              <Image
+                src={checkImg}
+                alt="check"
+                height={30}
+                width={30}
+                className="mr-2"
+              />
               <p>Disminuye tiempos de espera</p>
-
             </div>
-            <div className='flex flex-row items-center text-white pb-8' style={{marginLeft: 0}}>
-              <Image src={checkImg} alt='check' height={30} width={30} className='mr-2' />
+            <div
+              className="flex flex-row items-center text-white pb-8"
+              style={{ marginLeft: 0 }}
+            >
+              <Image
+                src={checkImg}
+                alt="check"
+                height={30}
+                width={30}
+                className="mr-2"
+              />
               <p>Mejora la experiencia de tus usuarios</p>
-
             </div>
-            <div className='flex flex-row items-center text-white pb-8'style={{marginLeft: 0}}>
-              <Image src={checkImg} alt='check' height={30} width={30} className='mr-2' />
+            <div
+              className="flex flex-row items-center text-white pb-8"
+              style={{ marginLeft: 0 }}
+            >
+              <Image
+                src={checkImg}
+                alt="check"
+                height={30}
+                width={30}
+                className="mr-2"
+              />
               <p>Utiliza la IA para agilizarte</p>
             </div>
           </div>
@@ -53,27 +82,28 @@ const Banner = () => {
         {/* <div className="w-full sm:w-1/3 md:w-1/3 h-screen sm:h-auto lg:h-full p-4 sm:p-8 flex items-center justify-center animation-container-banner second-title-compu">
           <AokiRocket />
         </div> */}
-        
+
         {/* Texto visible solo en móviles */}
         <div className="w-full sm:w-1/2 md:w-1/2 p-4 sm:p-8 flex flex-col items-center justify-center animation-container-banner sm:hidden">
           <p className="text-white text-lg sm:text-lg lg:text-lg font-bold mb-8 font-poppins text-center w-full sm:text-left">
-            <span className="gradient-text">Automatizarte</span> nunca fue tan <span className="gradient-text">fácil</span>
+            <span className="gradient-text">Automatizarte</span> nunca fue tan{" "}
+            <span className="gradient-text">fácil</span>
           </p>
           {/* <button className="w-60 text-white p-2 mb-2 mt-6 btn-bg-color">Empezá tu prueba gratuita</button> */}
-          <div className='btn-container'>
-          <BtnCallToAction
-          text={'Empezá tu prueba gratuita hoy'}
-          rocket={false}
-          className={"w-60 text-white p-2 mb-2 mt-6 btn-bg-color"}
-          />
+          <div className="btn-container">
+            <BtnCallToAction
+              text={"Empezá tu prueba gratuita hoy"}
+              rocket={false}
+              className={"w-60 text-white p-2 mb-2 mt-6 btn-bg-color"}
+            />
           </div>
-          <p className="text-white text-sm mb-4">Podes empezar en menos de 60 segundos</p>
+          <p className="text-white text-sm mb-4">
+            Podes empezar en menos de 60 segundos
+          </p>
         </div>
-
       </div>
-      </div>
+    </div>
+  );
+};
 
-    )
-}
-
-export default Banner
+export default Banner;
