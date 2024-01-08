5963bd3bcddb74272af365fde1a70b62d68377f4
bosxch <amparocardosobosch@gmail.com>
2024-01-08 09:56:37 -0300

    padding center

--- src/components/BannerComponent/BannerIndex.tsx
+++ src/components/BannerComponent/BannerIndex.tsx
@@ -7,21 +7,21 @@ import ReactPlayer from "react-player";
 import Video from "../VideoComponent/VideoComponentIndex";
 const Banner = () => {
   return (
-    <div className="banner-main-aoki flex flex-col h-full">
-      <div className="flex flex-col sm:flex-row h-100 w-100  p-8  justify-start banner-container bg-transparent" style={{paddingTop: '0vh'}}>
+    <div className="banner-main-aoki flex flex-col h-full" style={{paddingTop: '0vh'}}>
+      <div className="flex flex-col sm:flex-row h-100 w-100  p-8  justify-center banner-container bg-transparent" style={{paddingTop: '0vh'}}>
         {/* Sección del texto */}
-        <div className="w-full p-8 sm:p-8 flex flex-col items-center justify-start text-center text-container-banner" >
+        <div className="w-full p-8 sm:p-8 flex flex-col items-center justify-center text-center text-container-banner" >
           <p className="text-white text-xl sm:text-2xl lg:text-4xl font-bold mb-4 font-poppins text-center w-full sm:text-center">
             Querés <span className="gradient-text">innovación</span>, querés{" "}
             <span className="gradient-text">Aoki</span>
           </p>
-          <h1 className="text-white text-4xl sm:text-6xl lg:text-8xl font-bold mb-8 font-poppins text-center sm:text-center w-full">
+          <h1 className="text-white text-4xl sm:text-6xl lg:text-8xl font-bold  font-poppins text-center sm:text-center w-full">
             Subí de nivel <span className="gradient-text">tu empresa</span>
             <br />
             de la mano de la <span className="gradient-text">IA</span>
           </h1>
           <div className="second-title-compu">
-            <p className="text-white text-lg sm:text-lg lg:text-lg font-bold mb-8 font-poppins text-center w-full sm:text-center">
+            <p className="text-white text-lg sm:text-lg lg:text-lg font-bold  font-poppins text-center w-full sm:text-center">
               <span className="gradient-text">Automatizarte</span> nunca fue tan{" "}
               <span className="gradient-text">fácil</span>
             </p>
@@ -87,7 +87,7 @@ const Banner = () => {
 
         {/* Texto visible solo en móviles */}
         <div className="w-full sm:w-1/2 md:w-1/2 p-4 sm:p-8 flex flex-col items-center justify-center animation-container-banner sm:hidden">
-          <p className="text-white text-lg sm:text-lg lg:text-lg font-bold mb-8 font-poppins text-center w-full sm:text-left">
+          <p className="text-white text-lg sm:text-lg lg:text-lg font-bold font-poppins text-center w-full sm:text-left">
             <span className="gradient-text">Automatizarte</span> nunca fue tan{" "}
             <span className="gradient-text">fácil</span>
           </p>
